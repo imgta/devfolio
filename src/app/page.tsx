8ed73034fc283317c47276123c8c109281fd1a8e
Gordon Ta <gphamta+github@gmail.com>
2025-07-20 09:33:34 -0400

    typo fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Hero from '@/components/Hero';
 import TechStack from '@/components/TechStack';
-import AboutMe from '@//components/AboutMe';
+import AboutMe from '@/components/AboutMe';
 import Projects from '@/components/Projects';
 
 //------------------------------------------------------------
