579ac6dd31775d90cf50da2c92f898c8807136f9
Gordon Ta <gphamta+github@gmail.com>
2025-07-20 10:16:29 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias['@components'] = path.resolve(__dirname, 'src/app/components');
+        return config;
+    }
+};
+
+module.exports = nextConfig;
